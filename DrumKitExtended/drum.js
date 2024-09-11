fa4ce499cb2d28b66564679545eeac949d2595d2
srujan601 <127979115+srujan601@users.noreply.github.com>
2024-09-11 16:00:58 +0530

    Add files via upload

---
+++ DrumKitExtended/drum.js
@@ -0,0 +1,80 @@
+var lenOfBtn = document.querySelectorAll(".drum").length;
+
+for (var i = 0; i < lenOfBtn; i++) {
+    document.querySelectorAll(".drum")[i].addEventListener("click", function () {
+      var buttonInnerHtml = this.innerHTML;
+      makesound(buttonInnerHtml);
+      buttonAnimation(buttonInnerHtml);
+    });
+  }
+
+document.addEventListener("keydown", function (event) {
+  makesound(event.key.toLowerCase());
+  buttonAnimation(event.key.toLowerCase());
+  triggerWaveAnimation();
+},true);
+
+function makesound(key) {
+  switch (key) {
+    case "w":
+      var tom1 = new Audio("sounds/tom-1.mp3");
+      tom1.play();
+      break;
+    case "a":
+      var tom2 = new Audio("sounds/tom-2.mp3");
+      tom2.play();
+      break;
+    case "s":
+      var tom3 = new Audio("sounds/tom-3.mp3");
+      tom3.play();
+      break;
+    case "d":
+      var tom4 = new Audio("sounds/tom-4.mp3");
+      tom4.play();
+      break;
+    case "j":
+      var crash = new Audio("sounds/crash.mp3");
+      crash.play();
+      break;
+    case "k":
+      var kick = new Audio("sounds/kick-bass.mp3");
+      kick.play();
+      break;
+    case "l":
+      var snare = new Audio("sounds/snare.mp3");
+      snare.play();
+      break;
+    default:
+      console.log(key);
+  }
+}
+
+function buttonAnimation(currkey) {
+  var activebtn = document.querySelector("." + currkey);
+  if (activebtn) {
+    activebtn.classList.add("pressed");
+
+    setTimeout(function () {
+      activebtn.classList.remove("pressed");
+    }, 100);
+  }
+}
+
+// Trigger sound wave animation
+function triggerWaveAnimation() {
+  var bars = document.querySelectorAll('.bar');
+  bars.forEach(function(bar) {
+    bar.style.animation = 'wave 1s ease infinite';
+  });
+
+  setTimeout(function() {
+    bars.forEach(function(bar) {
+      bar.style.animation = '';
+    });
+  }, 1000);
+}
+
+// Theme toggle
+document.getElementById("theme-toggle").addEventListener("click", function () {
+  document.body.classList.toggle("dark-mode");
+});
